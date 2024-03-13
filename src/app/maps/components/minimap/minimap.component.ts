75537b5498e48c8ab893087b3efa2e3f98b003aa
Alonso <marenas@itsmarts.com.mx>
2024-03-13 11:42:21 -0600

    Primer Commit

---
+++ src/app/maps/components/minimap/minimap.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minimap',
+  templateUrl: './minimap.component.html',
+  styleUrl: './minimap.component.css'
+})
+export class MinimapComponent {
+
+}
