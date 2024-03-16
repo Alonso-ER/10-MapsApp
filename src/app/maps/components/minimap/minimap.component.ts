087991ba6edf9af362d90d67edaf7a5caad9d6c4
Alonso <marenas@itsmarts.com.mx>
2024-03-16 09:57:10 -0600

    Fin sección 21

--- src/app/maps/components/minimap/minimap.component.ts
+++ src/app/maps/components/minimap/minimap.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-minimap',
+  selector: 'map-minimap',
   templateUrl: './minimap.component.html',
   styleUrl: './minimap.component.css'
 })
 export class MinimapComponent {
+
+  @Input() lnglat?: [number, number];
+  @ViewChild('map')divMap?:ElementRef
+
+
+  ngAfterViewInit(){
+    if( !this.divMap?.nativeElement ) throw "Map Div not Found";
+    if( !this.lnglat ) throw "LngLat can't be null";
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lnglat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false,
+    });
+
+    new Marker()
+      .setLngLat( this.lnglat )
+      .addTo( map )
+
+  }
 
 }
